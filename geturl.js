fa3990b6474c50f237de23916d46e687d8b23aa3
李永洪 <132liyh@sina.com>
2019-06-14 13:15:46 +0800

    修复传入没有的值返回错误的情况

--- geturl.js
+++ geturl.js
@@ -1,13 +1,14 @@
 var get_url = function (url, data) {
-    var back_data = {};
-    url = url.substr(url.indexOf('?') + 1, url.length);
-    for (let key in data) {
-        back_data[data[key]] = '';
-
-        back_data[data[key]] = url.substr(url.indexOf(data[key])+data[key].length+1)
-       if(url.substr(url.indexOf(data[key])+data[key].length+1).indexOf("&")>-1){
-            back_data[data[key]] = url.substr(url.indexOf(data[key])+data[key].length+1).substr(0,url.substr(url.indexOf(data[key])+data[key].length+1).indexOf("&"));
-       }
-    }
-    return back_data; 
-}
+          var back_data = {};
+          url = url.substr(url.indexOf('?') + 1, url.length);
+          for (let key in data) {
+              back_data[data[key]] = '';
+              if(url.indexOf(data[key])>-1){
+                back_data[data[key]] = url.substr(url.indexOf(data[key])+data[key].length+1)
+                if(url.substr(url.indexOf(data[key])+data[key].length+1).indexOf("&")>-1){
+                      back_data[data[key]] = url.substr(url.indexOf(data[key])+data[key].length+1).substr(0,url.substr(url.indexOf(data[key])+data[key].length+1).indexOf("&"));
+                }
+              }
+          }
+          return back_data; 
+      }
